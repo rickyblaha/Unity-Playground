224609d66b6b37ea37aedefd37e4804e31a7ce87
Blaha, Ricky <ricky.blaha@teamaol.com>
2017-07-12 17:26:45 -0400

    Roll-a-ball tutorial completed.

---
+++ Roll-a-ball Tutorial/Assets/scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Called after Update (once per frame) so player is guaranteed to have moved.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-a-ball Tutorial/Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Called on the first frame that the script is active.
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	// Called before rendering every frame.
+	void Update () {
+		// Unused for now.
+	}
+	
+	// Called before performing any physics calculations.
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	// Called when first touch with another object happens.
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "YOU WIN!!!!";
+		}
+	}
+}
